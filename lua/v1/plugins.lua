45502b728b334cb1958a2b7726d12d5ed3793644
matthewanderson <manderson1@firstorion.com>
2023-07-25 15:24:32 -0500

    Add a plugins.lua file for initializing plugins

---
+++ lua/v1/plugins.lua
@@ -0,0 +1,18 @@
+-- Addtional Plugins
+lvim.plugins = {
+    -- Lua
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  -- Vim Script
+  {
+    "psliwka/vim-smoothie"
+  },
+}
