68978905ac385e463257099836f1c15a366606b9
matthewanderson <manderson1@firstorion.com>
2023-07-25 15:37:06 -0500

    Add an options.lua file for setting lunarvim options

---
+++ lua/v1/options.lua
@@ -0,0 +1,23 @@
+-- General Options
+lvim.log.level = "warn"
+lvim.format_on_save.enabled = false
+
+
+-- Set the Colorscheme
+-- Commented out are colorschemes I like.
+-- lvim.colorscheme = "primer_dark"
+lvim.colorscheme = "lunar"
+
+
+-- Builtin Options
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+
+lvim.builtin.terminal.active = true
+
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+lvim.builtin.breadcrumbs.active = true
+
+
