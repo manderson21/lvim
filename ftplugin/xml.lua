21726cbee11e7275607bba55be6fd14d5877f06f
matthewanderson <manderson1@firstorion.com>
2023-08-09 15:38:19 -0500

    Add maven functionality to lemminx

--- ftplugin/xml.lua
+++ ftplugin/xml.lua
@@ -1,12 +1,25 @@
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "lemminx" })
 
-local cacheLocation = "~/.cache/lemminx"
+local cacheLocation = "~/.cache/lemminx/"
+local lemminxLocation = "~/repos/community/lemminx/"
 
 local lsp_manager = require("lvim.lsp.manager")
 lsp_manager.setup("lemminx", {
-  cmd = { "lemminx" },
+  cmd = {
+    -- Command to execute the server:
+    "java",
 
-  -- Possible Settings are defined at: 
+    -- Comment out for Remote Debugging of the Lemminx LSP Server:
+    -- "-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005",
+
+    -- Path to lemminx jar.
+    "-cp", vim.fn.glob(lemminxLocation .. "lemminx-maven/lemminx-maven/target/vscode-lemminx-maven-jars/*"),
+
+    -- Main Class
+    "org.eclipse.lemminx.XMLServerLauncher"
+  },
+
+  -- Possible Settings are defined at:
   --    https://github.com/redhat-developer/vscode-xml/blob/main/docs/Preferences.md
   settings = {
     xml = {
